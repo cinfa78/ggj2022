7e6b8e45423bf13cb126eea1328f5aacf7cebfa5
cinfa <cinfa78@gmail.com>
2022-01-29 17:04:12 +0100

    activated object on text shown

--- GGJ-2022-TeamCheap/Assets/Scripts/ShowTextOnInteract.cs
+++ GGJ-2022-TeamCheap/Assets/Scripts/ShowTextOnInteract.cs
@@ -1,20 +1,26 @@
-using System;
 using System.Collections;
+using Sirenix.OdinInspector;
 using TMPro;
 using UnityEngine;
 
 public class ShowTextOnInteract : Interactable {
 	public Canvas canvasText;
 	public CanvasGroup iconCanvasGroup;
-	[TextArea]public string message;
+	[TextArea] public string message;
 	private CanvasGroup textCanvasGroup;
 	private TMP_Text label;
 	private int textLength;
 	public float duration = 0.3f;
 	public float minTimeout;
 	public float fadeDuration;
+	private Collider collider;
+	public GameObject[] objectsToActivate;
+	public GameObject[] objectsToDeactivate;
 	private Coroutine iconCoroutine;
 	private Coroutine textCoroutine;
+
+	public bool showTextMultipleTimes;
+	[ReadOnly] public bool storyAdvanced;
 
 	private void Awake() {
 		textCanvasGroup = canvasText.GetComponent<CanvasGroup>();
@@ -22,6 +28,10 @@ public class ShowTextOnInteract : Interactable {
 		label = canvasText.GetComponentInChildren<TMP_Text>();
 		textLength = label.text.Length;
 		iconCanvasGroup.alpha = 0;
+		foreach (GameObject o in objectsToActivate) {
+			o.SetActive(false);
+		}
+		collider = GetComponent<Collider>();
 	}
 
 	private void Start() {
@@ -29,8 +39,18 @@ public class ShowTextOnInteract : Interactable {
 	}
 
 	public override void Interact() {
-		if (textCoroutine != null) StopCoroutine(textCoroutine);
-		textCoroutine = StartCoroutine(FadeOutText());
+		if (showTextMultipleTimes) {
+			if (textCoroutine != null) StopCoroutine(textCoroutine);
+			textCoroutine = StartCoroutine(FadeOutText());
+		}
+		else {
+			if (!storyAdvanced) {
+				if (textCoroutine != null) StopCoroutine(textCoroutine);
+				textCoroutine = StartCoroutine(FadeOutTextOnce());
+				storyAdvanced = true;
+				Destroy(collider);
+			}
+		}
 	}
 
 	public override void ShowInteractionAvailable() {
@@ -70,5 +90,33 @@ public class ShowTextOnInteract : Interactable {
 			yield return null;
 		}
 		textCanvasGroup.alpha = 0;
+	}
+
+	private IEnumerator FadeOutTextOnce() {
+		textCanvasGroup.alpha = 0;
+		float timer = fadeDuration / 2;
+		while (timer > 0) {
+			float t = timer / fadeDuration;
+			textCanvasGroup.alpha = 1 - t;
+			timer -= Time.deltaTime;
+			yield return null;
+		}
+		textCanvasGroup.alpha = 1;
+		float timeout = minTimeout - fadeDuration / 2 + textLength * 0.1f;
+		yield return new WaitForSeconds(timeout);
+		timer = fadeDuration;
+		while (timer > 0) {
+			float t = timer / fadeDuration;
+			textCanvasGroup.alpha = t;
+			timer -= Time.deltaTime;
+			yield return null;
+		}
+		textCanvasGroup.alpha = 0;
+		foreach (GameObject o in objectsToActivate) {
+			o.SetActive(true);
+		}
+		foreach (GameObject o in objectsToDeactivate) {
+			o.SetActive(false);
+		}
 	}
 }
